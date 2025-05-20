deaa5a19c6aa7e5ae6143103a54232b975090cfb
Aidai Mashrapova <aidaimashrapova8@mail.ru>
2025-05-20 13:03:37 +0600

    First commit

---
+++ backend/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import expressWs from "express-ws";
+import cors from "cors";
+import WebSocket from "ws";
+
+const app = express();
+const wsInstance = expressWs(app);
+
+const port = 8000;
+app.use(cors());
+
+const router = express.Router();
+wsInstance.applyTo(router);
+
+const connectedClient: WebSocket[] = [];
+
+interface IncomingMessage {
+   type: string;
+   payload: string;
+}
+
+app.use(router);
+app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ frontend/src/types.d.ts
@@ -0,0 +1,9 @@
+export interface ChatMessage {
+    username: string;
+    text: string;
+}
+
+export interface IncomingMessage {
+    type: string;
+    payload: ChatMessage;
+}
